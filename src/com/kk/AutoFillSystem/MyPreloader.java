4d2523c2621179b63784dbcc87910f45a269b3f7
Yi Xue <yxuecs@gmail.com>
2016-09-15 18:54:15 -0400

    09/15 added kmart

--- src/com/kk/AutoFillSystem/MyPreloader.java
+++ src/com/kk/AutoFillSystem/MyPreloader.java
@@ -1,31 +1,51 @@
 package com.kk.AutoFillSystem;
 
-import javafx.application.Platform;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
 import javafx.application.Preloader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javax.swing.JOptionPane;
 
 public class MyPreloader extends Preloader {
     
     Stage stage;
     
     private Scene createPreloaderScene() {
+        //loading mf
+        Manifest mf = new Manifest();
+        Attributes atts;
+        String s = "MANIFEST.MF";
+        
+        String version = "1.0.0";
+                
+        InputStream inputStream = AutoFillSystem.class.getResourceAsStream(s);
+        try {
+            mf.read(inputStream);
+            atts = mf.getMainAttributes();
+            
+            version = atts.getValue("version");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        
+        
+        Label lblVersion = new Label("Ver. "+ version);
+        lblVersion.setPadding(new Insets(0, 0, 5, 0));
         Label title = new Label("Loading, please wait...");
         title.setTextAlignment(TextAlignment.CENTER);
         title.setPadding(new Insets(0, 0, 5, 0));
         
         VBox root = new VBox(title);
+        root.getChildren().add(lblVersion);
         root.setAlignment(Pos.BOTTOM_CENTER);
     
         String image =  getClass().getResource("splash.jpg").toExternalForm();
